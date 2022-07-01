ab6960d0095dbd955fced818ee1c8919c6361e6b
gInnaro <106085169+gInnaro@users.noreply.github.com>
2022-07-01 13:57:42 +0300

    Update Skladheroku.py

--- Skladheroku.py
+++ Skladheroku.py
@@ -79,12 +79,12 @@ async def passn(message: types.Message):
     await Form.brand_t.set()
     global msg_id
     msg_id = await bot.send_message(message.chat.id, 'Марка Автомобиля? ')
+    msg_id = msg_id.message_id
     
 @dp.message_handler(state=Form.brand_t)
 async def brand(message): #получаем марку Автомобиля
     global brand_t;
     brand_t = message.text;
-    msg_id = msg_id.message_id
     await bot.delete_message(chat_id=message.chat.id, message_id=int(msg_id + 1))
     await bot.edit_message_text(chat_id=message.chat.id, message_id=msg_id.message_id, text = f'Марка Автомобиля: \n{brand_t}')
     print('Марка: ' + brand_t);
